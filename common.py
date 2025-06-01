cfd234bff2aff6ad57ff26e35c3153cd6faa2eef
Keegan Smith <keeganasmith2003@gmail.com>
2025-05-31 19:34:06 -0500

    hw1 fine

---
+++ common.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+def split_by(df, col):
+    unique_col_values = df[col].unique()
+    df_dict = {}
+    for value in unique_col_values:
+        df_dict[value] = df.loc[df[col] == value]
+    return df_dict
+
+def standard_deviation(df, col):
+    return df.std(ddof = 1)[col]
+
+def median(df, col):
+    return df.median()[col]
+
+def find_outliers(df, col):
+    first_quantile = df[col].quantile(.25)
+    third_quantile = df[col].quantile(.75)
+    IQR = third_quantile - first_quantile
+    lower_bound = first_quantile - 1.5 * IQR
+    upper_bound = third_quantile + 1.5 * IQR
+    outliers = df.loc[(df[col] < lower_bound) | (df[col] > upper_bound)]
+    return outliers
+
+def get_percentile_inclusive(df, col, value):
+    sorted_values = df[col].sort_values()
+    count_leq = (sorted_values <= value).sum()
+    percentile = count_leq / len(sorted_values)
+    return percentile
+
+def get_percentile_exclusive(df, col, value):
+    sorted_values = df[col].sort_values()
+    count_le = (sorted_values < value).sum()
+    percentile = count_le / len(sorted_values)
+    return percentile
+
+def get_iqr(df, col):
+    first_quantile = df[col].quantile(.25)
+    third_quantile = df[col].quantile(.75)
+    IQR = third_quantile - first_quantile
+    return IQR
+
+def histogram(df, col):
+    x = df[col]
+    counts, bins = np.histogram(x)
+    plt.hist(bins[:-1], bins, weights=counts)
+    plt.show()
+
+def compute_z_score(value, mean, std):
+    return (value - mean) / std
+
+#P(x <= z_score)
+def cdf(z_score):
+    cdf = norm.cdf(z_score)
+    return cdf
--- hw1/main.py
+++ hw1/main.py
@@ -1,1 +1,87 @@
 import pandas as pd
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from common import standard_deviation, split_by, median, find_outliers, get_percentile_exclusive, get_iqr, histogram, compute_z_score, cdf
+blue_df = pd.read_csv("./hw1/bluebonnet.csv")
+
+df_dict = split_by(blue_df, "distance")
+near = df_dict[0]
+away = df_dict[1]
+near_std = standard_deviation(near, "redpetals")
+away_std = standard_deviation(away, "redpetals")
+print("near variation: ", near_std)
+print("away variation: ", away_std)
+print(blue_df.describe())
+
+print("median red petals near: ", median(near, "redpetals"))
+
+outliers = find_outliers(near, "redpetals")
+print(outliers)
+
+print("average red petals near: ", near["redpetals"].mean())
+print("average red petals away: ", away["redpetals"].mean())
+
+print("petals < 4: ", get_percentile_exclusive(near, "redpetals", 4))
+print("petals < 4 (away): ", get_percentile_exclusive(away, "redpetals", 4))
+
+sat_df = pd.read_csv("./hw1/sat_gender.csv")
+difference = sat_df["MaleVerbal"].median() - sat_df["FemaleVerbal"].median()
+print("male - female: ", difference)
+mean_difference = sat_df["MaleVerbal"].mean() - sat_df["FemaleVerbal"].mean()
+print("male - female (mean): ", mean_difference)
+
+print("male iqr: ", get_iqr(sat_df, "MaleVerbal"))
+print("female iqr: ", get_iqr(sat_df, "FemaleVerbal"))
+
+print("male std: ", standard_deviation(sat_df, "MaleVerbal"))
+print("female std: ", standard_deviation(sat_df, "FemaleVerbal"))
+
+print("male 90th: ", sat_df["MaleVerbal"].quantile(.9))
+print("female 90th: ", sat_df["FemaleVerbal"].quantile(.9))
+
+print("male outliers: ", find_outliers(sat_df, "MaleVerbal"))
+print("female outliers: ", find_outliers(sat_df, "FemaleVerbal"))
+
+surv_df = pd.read_csv("./hw1/heart_survival.csv")
+#histogram(surv_df, "StandardTherapy")
+print("standard outliers: ", find_outliers(surv_df, "StandardTherapy"))
+print("new outliers: ", find_outliers(surv_df, "NewTherapy"))
+
+print("standard less than 14 months: ", get_percentile_exclusive(surv_df, "StandardTherapy", 14))
+print("new less than 14 months: ", get_percentile_exclusive(surv_df, "NewTherapy", 14))
+
+stacked_df = pd.read_csv("./hw1/heart_survival_stack.csv")
+print("total less than 14 months: ", get_percentile_exclusive(stacked_df, "Survival", 14))
+
+rat_df = pd.read_csv("./hw1/adduct.csv")
+corn_df = split_by(rat_df, "diet")["corn"]
+fish_df = split_by(rat_df, "diet")["fish"]
+
+print("corn average: ", corn_df["distal"].mean())
+print("fish average: ", fish_df["distal"].mean())
+
+print("corn median: ", corn_df["distal"].median())
+print("fish median: ", fish_df["distal"].median())
+
+print("corn std: ", corn_df["distal"].std())
+print("fish std: ", fish_df["distal"].std())
+
+print("corn iqr: ", get_iqr(corn_df, "distal"))
+print("fish iqr: ", get_iqr(fish_df, "distal"))
+
+print("corn outliers: ", find_outliers(corn_df, "distal"))
+print("fish outliers: ", find_outliers(fish_df, "distal"))
+
+z_score = compute_z_score(33, 17, 8)
+cdf_33 = cdf(z_score)
+print(f"probability greater than {z_score}: ", 1 - cdf(z_score))
+z_score = compute_z_score(25, 17, 8)
+cdf_25 = cdf(z_score)
+print("probability between 25 and 33 is: ", cdf_33 - cdf_25)
+print("probability less than 25: ", cdf_25)
+print("original description: ", fish_df.describe())
+rat_new_df = pd.read_csv("./hw1/adduct2.csv")
+fish_new_df = split_by(rat_new_df, "diet")["fish"]
+print("new description: ", fish_new_df.describe())
